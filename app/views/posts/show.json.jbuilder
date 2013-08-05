39a087f682b80d81fd46f9ccd75efe7df63b92d1
Eitan Doron <edoron@vmware.com>
2013-08-05 16:19:36 -0700

    Initialize Repository

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :name, :address, :dob, :desire, :interests, :hobby, :signature, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TraveLog::Application.config.secret_key_base = '154abea99900c7b64b9e980c712eb84a16ae3f77555773d3a88df72ac9f02cdb3069c7247b2b8d3f49a89dd67aae7c84d0e2bc3fcc39563467789c88be496f2e'
